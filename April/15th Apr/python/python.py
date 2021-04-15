61d53af2b216a718bcec884374148d8c31e82c5e
Akshay Sahu <akki5233@gmail.com>
2021-04-15 18:03:24 +0530

    Python basics

---
+++ April/15th Apr/python/python.py
@@ -0,0 +1,16 @@
+# Operators
+
+# + - Addition
+# - - Substraction
+# / - Division
+# * - Multiplication
+# % - Modulo
+# // - Integer division
+# **  - exponent operator
+num1 = 8
+num2 = 3
+print(num1/num2);
+print(num1%num2);
+print(num1//num2)
+print(num1**num2)
+
